549bb47c835b20d61a6ed401c68bd259e5d7bd06
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-26 12:34:24 +0530

    feat(Server) - Server configuration complete

---
+++ node_postgres/first_server/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+//creates a server on the http(hyper text transfer protocall) format
+const hostname = 'localhost';
+//hostname allows us to identify the server on different web apps eg. youtube.com ect..
+const port = 3000;
+//portnumber for the server to shkow running processes
+const server = http.createServer((request ,response) => {
+    response.end('Welcome to Node!');
+})
+//createServer is a function to handle incoming request from the client and give back a response
+server.listen(port, hostname, () => {
+    console.log(`Server running at ${hostname}: ${port}`);
+});
+//starts a server up to listen to requests by providing our specified port and hostname
