8de36b6de6de2b9cb342877c076be83f1c87b823
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-26 12:36:45 +0530

    feat(Server) - Changed Server configuration

--- node_postgres/first_server/index.js
+++ node_postgres/first_server/index.js
@@ -5,7 +5,7 @@ const hostname = 'FirstServe';
 const port = 4100;
 //portnumber for the server to shkow running processes
 const server = http.createServer((request ,response) => {
-    response.end('Welcome to Node!');
+    response.end('Welcome to Node my friends');
 })
 //createServer is a function to handle incoming request from the client and give back a response
 server.listen(port, hostname, () => {
