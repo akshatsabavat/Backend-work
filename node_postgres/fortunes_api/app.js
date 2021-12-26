7f1c50a85f2fc75f7fa7066497dddb1a0e20f13a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2021-12-26 20:45:07 +0530

    feat(API) - renamed to app.js

--- node_postgres/fortunes_api/app.js
+++ node_postgres/fortunes_api/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fortunes = require('./data/fortunes.json');
 //creates and stores the express model in the express variable 
-const port = 3000;
 const app = express();
 //creates the express application using the express function
+const port = 3000;
 
 app.get('/fortunes', (req, res) => {
     res.json(fortunes);
@@ -11,5 +11,5 @@ app.get('/fortunes', (req, res) => {
 //allows us to specify the endpoint and access fires the call back function after the endpoint is called
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+    console.log(`Running at port : ${port}`);
+})
