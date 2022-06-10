b1bb86066e45e127a651b2f48d01555c3725733c
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-10 13:23:08 +0530

    redone code

--- node_postgres/first_server/index.js
+++ node_postgres/first_server/index.js
@@ -1,24 +1,23 @@
-const http = require('http');
+const http = require("http");
 //creates a server on the http(hyper text transfer protocall) format
-const hostname = 'localhost';
+const hostname = "localhost";
 //hostname allows us to identify the server on different web apps eg. youtube.com ect..
 const port = 3000;
 //portnumber for the server to shkow running processes
-const server = http.createServer((request ,response) => {
-    const {url} = request;
-    console.log(url);
+const server = http.createServer((request, response) => {
+  const { url } = request;
+  console.log(url);
 
-    if(url === "/translations") {
-        response.end("Your on translations");
-    }
-
-    if(url === "/testEndPoint"){
-        response.end("Your on test endpoint");
-    }
-    response.end('Welcome to Node!');
-})
+  if (url === "/translations") {
+    response.write("Your on translations");
+  } else if (url === "/cars") {
+    response.write("Your on cars");
+  }
+  response.write("Welcome to the Page!");
+  response.end();
+});
 //createServer is a function to handle incoming request from the client and give back a response
 server.listen(port, hostname, () => {
-    console.log(`Server running at ${hostname}: ${port}`);
+  console.log(`Server running at ${hostname}: ${port}`);
 });
 //starts a server up to listen to requests by providing our specified port and hostname
