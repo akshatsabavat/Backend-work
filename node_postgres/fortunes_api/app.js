1bc8c1e792e98c9d46ea359eff76fc04844841f2
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-11 18:19:02 +0530

    basic API made

--- node_postgres/fortunes_api/app.js
+++ node_postgres/fortunes_api/app.js
@@ -1,1 +1,24 @@
- 
+const fortuneData = require("./data/fortunes.json");
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.get("/fortunes", (req, res) => {
+  res.json(fortuneData);
+});
+
+app.get("/fortunes/random", (req, res) => {
+  randomFortune = Math.floor(Math.random() * 3);
+  res.json(fortuneData[randomFortune].message);
+});
+
+app.get("/fortunes/:fortune_id", (req, res) => {
+  res.json(
+    fortuneData.filter((fortune) => fortune.id == req.params.fortune_id)
+  );
+});
+
+app.listen(port, () => {
+  console.log(`listening on port number: ${port}`);
+});
